ab077a109214c3930d526f46f8f7acb5f8597c80
PrasetyoBayu07 <102822180+PrasetyoBayu07@users.noreply.github.com>
2025-10-29 01:42:38 +0700

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+// rollup.config.js
+import { terser } from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/dsrt.js',
+  output: [
+    {
+      file: 'dist/dsrt.js',
+      format: 'umd',
+      name: 'DSRT',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/dsrt.min.js',
+      format: 'umd',
+      name: 'DSRT',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env'],
+    }),
+  ],
+};
